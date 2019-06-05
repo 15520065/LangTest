946fec46b3b1af47744eada858cad01f89eae10e
HuyDaoDuc <15520295@gm.uit.edu.vn>
2019-06-05 15:28:40 +0700

    Add question Reading - Listening (part1, part2)

--- Questions/Listening_Step1.tsx
+++ Questions/Listening_Step1.tsx
@@ -2,6 +2,8 @@ import IQuestion, {QuestionType} from "../src/entity/Question";
 
 //TODO: Thêm hình Cho Part 1, Part 2, Part 4, Part 5
 
+const path = ""
+
 const QUESTION_PHASE: string = 'Listen and circle the correct answers.'
 const QuestionDataPart1: IQuestion[] = [{
     id: 'step1_1_002',
@@ -9,8 +11,8 @@ const QuestionDataPart1: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 1,
-    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    imageAsset: require('path/images/test1/p1_1_001.png'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }, {
     id: 'step1_1_003',
@@ -18,7 +20,7 @@ const QuestionDataPart1: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 2,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }, {
     id: 'step1_1_004',
@@ -26,7 +28,7 @@ const QuestionDataPart1: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 0,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }];
 
@@ -37,7 +39,7 @@ const QuestionDataPart2: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 1,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }, {
     id: 'step1_1_006',
@@ -45,7 +47,7 @@ const QuestionDataPart2: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 2,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }, {
     id: 'step1_1_007',
@@ -53,7 +55,7 @@ const QuestionDataPart2: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 0,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }];
 
@@ -63,7 +65,7 @@ const QuestionDataPart3: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 1,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }, {
     id: 'step1_1_009',
@@ -71,7 +73,7 @@ const QuestionDataPart3: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 2,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }, {
     id: 'step1_1_0010',
@@ -79,7 +81,7 @@ const QuestionDataPart3: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 0,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }];
 
@@ -89,7 +91,7 @@ const QuestionDataPart4: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 1,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }, {
     id: 'step1_1_0012',
@@ -97,7 +99,7 @@ const QuestionDataPart4: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 2,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }];
 
@@ -107,7 +109,7 @@ const QuestionDataPart5: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 1,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }, {
     id: 'step1_1_0014',
@@ -115,7 +117,7 @@ const QuestionDataPart5: IQuestion[] = [{
     question: QUESTION_PHASE,
     answer: ['A', 'B', 'C'],
     correctAnswer: 2,
-    audioAsset: require('C:\Users\huydd\Downloads\Documents\TOEFL PRIMARY-20190506T064336Z-001\TOEFL PRIMARY\Step 1\Toefl Primary Step 1 - Book1'),
+    audioAsset: require('path/Step 1/Toefl Primary Step 1 - Book1'),
     difficultLevel: 3
 }];
 export default QuestionDataPart1;
--- Questions/Reading_Step1.tsx
+++ Questions/Reading_Step1.tsx
@@ -1,12 +1,120 @@
-import IQuestion, {QuestionType} from "../entity/Question";
+import IQuestion, {QuestionType} from "../src/entity/Question";
 
-//TODO: Edit imageAsset for Part 1, Part 2
+//TODO: Edit imageAsset for Part 1
 const QUESTION_PHASE_PART1_123: string = 'Choose the word that goes best with each picture'
 const QUESTION_PHASE_PART1_456: string = 'What does the arrow show?'
 const QUESTION_PHASE_PART1_789: string = 'Choose the sentence that goes best with each picture'
 
-const QuestionDataPart1: IQuestion[] = [{
-    id: 'p1_1_001',
+const QuestionReadingPart1: IQuestion[] = [{
+    id: 'r_s1_b1_u1_p1_1',
+    type: QuestionType.part4,
+    question: QUESTION_PHASE_PART1_123,
+    answer: [
+        'Grandmother',
+        'Grandfather',
+        'Aunt'],
+    correctAnswer: 1,
+    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
+    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+    difficultLevel: 3
+}, {
+    id: 'r_s1_b1_u1_p1_2',
+    type: QuestionType.part4,
+    question: QUESTION_PHASE_PART1_123,
+    answer: [
+        'Younger sister',
+        'Younger brother',
+        'Older brother'],
+    correctAnswer: 0,
+    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
+    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+    difficultLevel: 3
+}, {
+    id: 'r_s1_b1_u1_p1_3',
+    type: QuestionType.part4,
+    question: QUESTION_PHASE_PART1_123,
+    answer: [
+        'Divorced',
+        'Separated',
+        'Married'],
+    correctAnswer: 2,
+    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
+    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+    difficultLevel: 3
+}, {
+    id: 'r_s1_b1_u1_p1_4',
+    type: QuestionType.part4,
+    question: QUESTION_PHASE_PART1_123,
+    answer: [
+        'Family',
+        'Friends',
+        'Co-workers'],
+    correctAnswer: 1,
+    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
+    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+    difficultLevel: 3
+}, {
+    id: 'r_s1_b1_u1_p1_5',
+    type: QuestionType.part4,
+    question: QUESTION_PHASE_PART1_123,
+    answer: [
+        'Parents',
+        'Sisters',
+        'Brothers '],
+    correctAnswer: 1,
+    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
+    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+    difficultLevel: 3
+}, {
+    id: 'r_s1_b1_u1_p1_6',
+    type: QuestionType.part4,
+    question: QUESTION_PHASE_PART1_123,
+    answer: [
+        'She is my big sister',
+        'He is my little brother.',
+        'He is my big brother.  '],
+    correctAnswer: 1,
+    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
+    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+    difficultLevel: 3
+}, {
+    id: 'r_s1_b1_u1_p1_7',
+    type: QuestionType.part4,
+    question: QUESTION_PHASE_PART1_123,
+    answer: [
+        'There are five people in my family',
+        'There are six people in my family.',
+        'There are four people in my family'],
+    correctAnswer: 0,
+    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
+    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+    difficultLevel: 3
+}, {
+    id: 'r_s1_b1_u1_p1_8',
+    type: QuestionType.part4,
+    question: QUESTION_PHASE_PART1_123,
+    answer: [
+        'A boy is with his brother.',
+        'A boy is with his grandmother.',
+        'A boy is with his dad'],
+    correctAnswer: 1,
+    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
+    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+    difficultLevel: 3
+}, {
+    id: 'r_s1_b1_u1_p1_9',
+    type: QuestionType.part4,
+    question: QUESTION_PHASE_PART1_123,
+    answer: [
+        'An old man is hugging his grandson',
+        'An old man is looking at his granddaughter',
+        'An old woman is talking to her grandson'],
+    correctAnswer: 0,
+    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
+    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+    difficultLevel: 3
+}, {
+    id: 'r_s1_b1_u2_p1_1',
     type: QuestionType.part4,
     question: QUESTION_PHASE_PART1_123,
     answer: [
@@ -18,7 +126,7 @@ const QuestionDataPart1: IQuestion[] = [{
     comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
     difficultLevel: 3
 }, {
-    id: 'p1_1_002',
+    id: 'r_s1_b1_u2_p1_2',
     type: QuestionType.part4,
     question: QUESTION_PHASE_PART1_123,
     answer: [
@@ -30,7 +138,7 @@ const QuestionDataPart1: IQuestion[] = [{
     comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
     difficultLevel: 3
 }, {
-    id: 'p1_1_003',
+    id: 'r_s1_b1_u2_p1_3',
     type: QuestionType.part4,
     question: QUESTION_PHASE_PART1_123,
     answer: [
@@ -42,7 +150,7 @@ const QuestionDataPart1: IQuestion[] = [{
     comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
     difficultLevel: 3
 }, {
-    id: 'p1_1_004',
+    id: 'r_s1_b1_u2_p1_4',
     type: QuestionType.part4,
     question: QUESTION_PHASE_PART1_456,
     answer: [
@@ -54,7 +162,7 @@ const QuestionDataPart1: IQuestion[] = [{
     comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
     difficultLevel: 3
 }, {
-    id: 'p1_1_005',
+    id: 'r_s1_b1_u2_p1_5',
     type: QuestionType.part4,
     question: QUESTION_PHASE_PART1_456,
     answer: [
@@ -66,7 +174,7 @@ const QuestionDataPart1: IQuestion[] = [{
     comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
     difficultLevel: 3
 }, {
-    id: 'p1_1_006',
+    id: 'r_s1_b1_u2_p1_6',
     type: QuestionType.part4,
     question: QUESTION_PHASE_PART1_456,
     answer: [
@@ -78,7 +186,7 @@ const QuestionDataPart1: IQuestion[] = [{
     comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
     difficultLevel: 3
 }, {
-    id: 'p1_1_007',
+    id: 'r_s1_b1_u2_p1_7',
     type: QuestionType.part4,
     question: QUESTION_PHASE_PART1_789,
     answer: [
@@ -90,7 +198,7 @@ const QuestionDataPart1: IQuestion[] = [{
     comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
     difficultLevel: 3
 }, {
-    id: 'p1_1_008',
+    id: 'r_s1_b1_u2_p1_8',
     type: QuestionType.part4,
     question: QUESTION_PHASE_PART1_789,
     answer: [
@@ -102,7 +210,7 @@ const QuestionDataPart1: IQuestion[] = [{
     comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
     difficultLevel: 3
 }, {
-    id: 'p1_1_009',
+    id: 'r_s1_b1_u2_p1_9',
     type: QuestionType.part4,
     question: QUESTION_PHASE_PART1_789,
     answer: [
@@ -116,42 +224,104 @@ const QuestionDataPart1: IQuestion[] = [{
 }];
 
 //
-const QuestionDataPart2: IQuestion[] = [{
-    id: 'p2_1_001',
-    type: QuestionType.part4,
-    question: 'Read the hint and choose the best answers',
+const QuestionReadingPart2: IQuestion[] = [{
+    id: 'r_s1_b1_u1_p2_1',
+    type: QuestionType.part3,
+    question: 'This is a group of people. They are related to each other. The members can be people such as grandparents, parents, sisters, brothers, and cousins.\n' +
+        'Q. Who are they?\n',
     answer: [
-        'A freckle',
-        'Glasses',
-        'A mustache'],
-    correctAnswer: 0,
-    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
-    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
-    difficultLevel: 3
+        'Classmates',
+        'Co-workers',
+        'Family'],
+    correctAnswer: 2,
+    difficultLevel: 3,
+    comeWith: ['p3_1_032', 'p3_1_033', 'p3_1_034']
 }, {
-    id: 'p2_1_002',
-    type: QuestionType.part4,
-    question: 'Read the hint and choose the best answers',
+    id: 'r_s1_b1_u1_p2_2',
+    type: QuestionType.part3,
+    question: 'This person is the mother of your mother or father. \n' +
+        'Q. Who is she? \n',
     answer: [
-        'A scar',
-        'A beard',
-        'A wrinkle'],
-    correctAnswer: 0,
-    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
-    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
-    difficultLevel: 3
+        'My grandfather',
+        'My mother',
+        'My grandmother'],
+    correctAnswer: 2,
+    difficultLevel: 3,
+    comeWith: ['p3_1_032', 'p3_1_033', 'p3_1_034']
 }, {
-    id: 'p2_1_003',
-    type: QuestionType.part4,
-    question: 'Read the hint and choose the best answers',
+    id: 'r_s1_b1_u1_p2_3',
+    type: QuestionType.part3,
+    question: 'This person is the brother of your mother or father.\n' +
+        'Q. Who is he?\n',
     answer: [
-        'Curly hair',
-        'A dimple',
-        'A ponytail'],
-    correctAnswer: 0,
-    imageAsset: require('./../../assets/images/test1/p1_1_001.png'),
-    comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
-    difficultLevel: 3
-}];
+        'My brother',
+        'My uncle',
+        'My nephew'],
+    correctAnswer: 1,
+    difficultLevel: 3,
+    comeWith: ['p3_1_032', 'p3_1_033', 'p3_1_034']
+}, {
+    id: 'r_s1_b1_u1_p2_4',
+    type: QuestionType.part3,
+    question: 'This person is the daughter of your brother or sister.\n' +
+        'Q. Who is she?\n',
+    answer: [
+        'My aunt',
+        'My daughter',
+        'My niece '],
+    correctAnswer: 2,
+    difficultLevel: 3,
+    comeWith: ['p3_1_032', 'p3_1_033', 'p3_1_034']
+},
+    // Unit 2
+    {
+        id: 'r_s1_b1_u2_p2_1',
+        type: QuestionType.part3,
+        question: 'It is a small brown spot on someone’s face.\n' +
+            'Q. What is it?\n',
+        answer: [
+            'My aunt',
+            'My daughter',
+            'My niece '],
+        correctAnswer: 2,
+        difficultLevel: 3,
+        comeWith: ['p3_1_032', 'p3_1_033', 'p3_1_034']
+    }, {
+        id: 'r_s1_b1_u2_p2_2',
+        type: QuestionType.part3,
+        question: '2.\tIt is a line on your face. You get this when you get old.\n' +
+            'Q. What is it?\n',
+        answer: [
+            'A scar',
+            'A beard',
+            'A wrinkle'],
+        correctAnswer: 0,
+        comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+        difficultLevel: 3
+    }, {
+        id: 'r_s1_b1_u2_p2_3',
+        type: QuestionType.part3,
+        question: 'Some people show a small hollow area on their cheek or chin when they smile.\n' +
+            'Q. What is it?\n',
+        answer: [
+            'Curly hair',
+            'A dimple',
+            'A ponytail'],
+        correctAnswer: 0,
+        comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+        difficultLevel: 3
+    }, {
+        id: 'r_s1_b1_u2_p2_4',
+        type: QuestionType.part3,
+        question: 'For this hairstyle, you cut the front part of your hair short.\n' +
+            'Q. What is it?\n',
+        answer: [
+            'A pigtail',
+            'Bangs',
+            'Bald '],
+        correctAnswer: 0,
+        comeWith: ['p4_1_071', 'p4_1_072', 'p4_1_073'],
+        difficultLevel: 3
+    }];
 
-export default QuestionDataPart1;
+export default QuestionReadingPart1;
